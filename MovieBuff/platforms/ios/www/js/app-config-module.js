fadb459eafb6a036fe7be8047c5367b8fa3aacfc
Eric Smith <motionsmith@gmail.com>
2014-08-14 14:32:49 -0700

    attempt to gitignore platforms folder

--- MovieBuff/platforms/ios/www/js/app-config-module.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	var appModule = angular.module('appConfig', []);
-
-	appModule.factory('apiUrl', function() {
-		return "http://minion.fayve.com/api/";
-		//return "http://www.fayve.com/api/";
-	});
-})()
